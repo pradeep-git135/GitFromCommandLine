69f665fe30d91b662e8a474486d981efc906e441
pradeep-git135 <pradeepcr135@gmail.com>
2021-10-29 11:13:11 +0530

    pull code in different workspace

--- src/javacode/ClassForNewBranch.java
+++ src/javacode/ClassForNewBranch.java
@@ -4,7 +4,7 @@ public class ClassForNewBranch {
 
 	public static void main(String[] args) {
 		System.out.println("push this class to newbranch");
-
+		System.out.println("Pull the code to different workspace");
 	}
 
 }
